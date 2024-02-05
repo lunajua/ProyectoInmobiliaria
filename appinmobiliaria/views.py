5e91fe3210be88d19dae260f7a56963558c67d62
Juan Pablo Luna <jp_luna@hotmail.com>
2024-02-05 20:06:09 -0300

    subida con index modificado

--- appinmobiliaria/views.py
+++ appinmobiliaria/views.py
@@ -3,10 +3,10 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html') 
+    return render(request, 'appinmobiliaria/index.html') 
 
 def about(request):
-    return render(request, 'about.html')
+    return render(request, 'appinmobiliaria/about.html')
 
 
 
