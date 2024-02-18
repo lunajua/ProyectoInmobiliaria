8fb65e6ed9451624ddc9292e4f51815c507abe79
Matias Wullich <wullichm@gmail.com>
2024-02-18 10:40:17 -0300

    creacion de modelos y superusuario

--- appinmobiliaria/admin.py
+++ appinmobiliaria/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+admin.site.register(Propiedad)
+admin.site.register(Cliente)
+admin.site.register(Agentes)
+admin.site.register(Testimonios)
+admin.site.register(Contacto)
+admin.site.register(Servicios)
+admin.site.register(Noticias)
--- appinmobiliaria/models.py
+++ appinmobiliaria/models.py
@@ -1,3 +1,51 @@
 from django.db import models
 
 # Create your models here.
+
+class Propiedad(models.Model):
+    direccion = models.CharField(max_length=40)
+    tipo_de_propiedad = models.CharField(max_length=40)
+    localizacion = models.CharField(max_length=40)
+    palabra_clave =models.CharField(max_length=40)
+    metros = models.IntegerField()
+    ambientes = models.IntegerField() 
+    banos = models.IntegerField()
+    descripcion = models.CharField(max_length=40)
+    venta_o_alquiler = models.BooleanField()
+    otros_atributos = models.CharField(max_length=40)
+
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=40)
+    apellido = models.CharField(max_length=40)
+    telefono = models.CharField(max_length=40)
+    email = models.EmailField(max_length=40)
+    preferencias = models.CharField(max_length=40)
+
+class Agentes(models.Model):
+    nombre = models.CharField(max_length=40)
+    apellido = models.CharField(max_length=40)
+    telefono = models.CharField(max_length=40)
+    email = models.EmailField(max_length=40)
+    especialidad = models.CharField(max_length=40)
+
+class Testimonios(models.Model):
+    nombre = models.CharField(max_length=40)
+    apellido = models.CharField(max_length=40)
+    telefono = models.CharField(max_length=40)
+    email = models.EmailField(max_length=40)
+    testimonios = models.CharField(max_length=40)
+
+class Contacto(models.Model):
+    nombre = models.CharField(max_length=40)
+    apellido = models.CharField(max_length=40)
+    telefono = models.CharField(max_length=40)
+    email = models.EmailField(max_length=40)
+    agendar_visita = models.BooleanField()
+
+class Servicios(models.Model):
+    nombre = models.CharField(max_length=40)
+    servicio = models.CharField(max_length=40)
+
+class Noticias(models.Model):
+    fecha = models.DateField()
+    noticia = models.CharField(max_length=40)
