f6ac8538aa75451e54101f0a31c35eb2fe4751ec
Lucas <lucasbecca3@hotmail.com>
2024-02-21 22:37:47 -0300

    app-name, vistas y urls

---
+++ users/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from users.views import CreacionDeCuenta, Login, logout
+
+app_name = 'users'
+
+urlpatterns = []
--- users/views.py
+++ users/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+class CreacionDeCuenta(generic.CreateView):
+    form_class = UserForm
+    success_url = 'pre_entrega/index.html'
+    template_name='pre_entrega/index.html'
+
+
+class Login(LoginView):
+    template_name = 'users/login.html'
+
+    def get_success_url(self):
+        return reverse_lazy('pre_entrega:index')
+    
+
+class logout(LogoutView):
+    next_page = 'pre_entrega:index'
