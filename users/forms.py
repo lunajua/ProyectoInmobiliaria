fddd5f5ed3df8102fc1ef5d178187570e9727e04
Matias Wullich <wullichm@gmail.com>
2024-02-21 22:35:21 -0300

    actualización

--- users/forms.py
+++ users/forms.py
@@ -1,1 +1,14 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class UserForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    password1 = forms.CharField(label='contraseña', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='repetir contraseña', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('username' , 'email' , 'password1', 'password2')
+        help_texts = {k:"" for k in fields}
+
