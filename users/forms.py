5c57f6da380906a3e3223fd7353a06cc3aaba4ef
Matias Wullich <wullichm@gmail.com>
2024-02-24 00:03:22 -0300

    cambio de contraseña y cambios en html

--- users/forms.py
+++ users/forms.py
@@ -12,3 +12,13 @@ class UserForm(UserCreationForm):
         fields = ('username' , 'email' , 'password1', 'password2')
         help_texts = {k:"" for k in fields}
 
+class UserEditForm(UserCreationForm):
+
+    email = forms.EmailField(label="Ingresar E-mail:")
+    password1 = forms.CharField(label='Contraseña:', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Repetir la contraseña:', widget=forms.PasswordInput) 
+
+    class Meta:
+        model = User
+        fields = [ 'email', 'password1', 'password2'] 
+        help_texts = {k:"" for k in fields}
